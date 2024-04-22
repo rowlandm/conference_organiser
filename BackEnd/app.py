e08e8c873d981882f267709592ec9c57e4480cf9
Yanhao Xu <51267436+HAOOO99@users.noreply.github.com>
2024-04-22 20:00:23 +0800

    Update app.py

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -24,11 +24,5 @@ async def get_user(user_id:int, db:Session = Depends(get_db)):
     
     return result
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello Aliens!!"}
-    
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+
 
