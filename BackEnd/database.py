91b11f13957d9eebad20ab88e779f4043e51a0f6
HAOOO99 <xuyanhao2000@gmail.com>
2024-04-22 15:59:26 +1000

    test vm connection

--- BackEnd/database.py
+++ BackEnd/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+
 URL_DATABASE = "postgresql://postgres:postgres@203.101.227.107:5432/mydb"
 
 engine = create_engine(URL_DATABASE)
