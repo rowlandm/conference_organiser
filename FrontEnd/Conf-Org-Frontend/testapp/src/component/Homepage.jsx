b79b64ee1bf8c81597f1ea82693035adbc658e82
Mingyang Li(Dennis) <limingycqyw@gmail.com>
2024-04-22 23:54:36 +1000

    update template_edit button

--- FrontEnd/Conf-Org-Frontend/testapp/src/component/Homepage.jsx
+++ FrontEnd/Conf-Org-Frontend/testapp/src/component/Homepage.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const HomePage = () => {
-
     const navigate = useNavigate();
+
     return (
         <div>
             <h1>Welcome to Our Application!</h1>
             <p>This is the homepage of your React application. Feel free to explore and sign in to access more features.</p>
-            <button onClick={navigate('/TemplateEdit')}>Template Edit</button>
+            {/* Correct the onClick event handler to use an arrow function */}
+            <button onClick={() => navigate('/TemplateEdit')}>Template Edit</button>
         </div>
     );
 };
