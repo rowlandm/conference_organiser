d651c1b76e912c219fda6acc20c94d3bf4945e7e
HAOOO99 <xuyanhao2000@gmail.com>
2024-04-25 15:53:09 +0000

    candidate and role database mode

--- BackEnd/models.py
+++ BackEnd/models.py
@@ -1,4 +1,6 @@
 from sqlalchemy import Boolean, Column, ForeignKey,Integer,String
+from sqlalchemy.orm import relationship
+
 from database import Base
 
 class User(Base):
@@ -8,3 +10,28 @@ class User(Base):
     user_email = Column(String,index = True)
     password = Column(String,index = True)
     role = Column(String, index = True)
+
+    
+
+class Candidate(Base):
+    __tablename__ = 'Candidate'
+    id = Column(Integer, primary_key = True, index = True, )
+    candidate_name = Column(String)
+    candidate_email = Column(String)
+    candidate_org = Column(String)
+    candidate_origin = Column(String)
+    reach_difficulty  = Column(Integer)
+    subscribe = Column(Boolean)
+
+    roles = relationship("Role", back_populates = "rid")
+
+class Role(Base):
+    __tablename__ = 'Role'
+    rid = Column(Integer, primary_key = True)
+    role = Column(String, primary_key = True)
+    status = Column(String)
+    rank = Column(Integer)
+    area = Column(String)
+    cid = Column(Integer, ForeignKey(Candidate.id))
+
+    candis = relationship("Candidate", back_populates = "id")
